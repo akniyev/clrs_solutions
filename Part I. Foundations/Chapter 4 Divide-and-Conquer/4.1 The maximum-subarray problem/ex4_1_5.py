c32884bb601e6b1233af5b7dedd0e1307560ea90
Hasan Akniyev <hasan.akniyev@gmail.com>
2019-10-13 04:40:10 +0300

    Ex 4.1-5 Bad implementation

--- Part I. Foundations/Chapter 4 Divide-and-Conquer/4.1 The maximum-subarray problem/ex4_1_5.py
+++ Part I. Foundations/Chapter 4 Divide-and-Conquer/4.1 The maximum-subarray problem/ex4_1_5.py
@@ -5,48 +5,44 @@ import time
 
 def linear_time_maximum_subarray(arr: list) -> (int, int, float):
     n = len(arr)
+    current_max_sum = arr[0]
     min_i = 0
     max_i = 0
-    max_sum = arr[0]
-    current_sum = max_sum
-    current_min_i = min_i
+
+    sum_from_min_to_j = arr[0]
+    max_sum_to_j = arr[0]
+    max_to_j_min = 0
 
     for j in range(1, n):
-        current_sum += arr[j]
+        sum_from_min_to_j += arr[j]
 
-        max_sum1 = current_sum
-        for j1 in range(current_min_i, j):
-            max_sum1 -= arr[j1]
-            if max_sum1 > current_sum:
-                current_min_i = j1
-                current_sum = max_sum1
+        max_sum_to_j += arr[j]
+        max_to_j1 = max_sum_to_j
+        for j1 in range(max_to_j_min, j):
+            max_to_j1 -= arr[j1]
+            if max_to_j1 > max_sum_to_j:
+                max_sum_to_j = max_to_j1
+                max_to_j_min = j1 + 1
 
-        v1 = max_sum
-        v2 = current_sum
-        v3 = arr[j]
-
-        if v2 > v1 and v2 > v3:
-            min_i = current_min_i
+        if max_sum_to_j >= sum_from_min_to_j and max_sum_to_j >= current_max_sum:
+            current_max_sum = max_sum_to_j
+            sum_from_min_to_j = max_sum_to_j
+            min_i = max_to_j_min
             max_i = j
-            max_sum = current_sum
-
-        elif v3 > v1 and v3 > v2:
-            min_i = j
+        elif sum_from_min_to_j >= max_sum_to_j and sum_from_min_to_j >= current_max_sum:
+            current_max_sum = sum_from_min_to_j
             max_i = j
-            max_sum = arr[j]
-            current_sum = arr[j]
 
-
-    return min_i, max_i, max_sum
+    return min_i, max_i, current_max_sum
 
 
 if __name__ == "__main__":
-    array_length = 10
+    array_length = 5000
 
     test_array = [random.randint(-100, 100) for _ in range(array_length)]
-    test_array = [97, -14, -84, -64, -33, 63, -29, 73, 99, -57]
+    # test_array = [97, -14, -84, -64, -33, 63, -29, 73, 99, -57]
 
-    print(test_array)
+    # print(test_array)
 
     start1 = time.time()
     ans1 = ex4_1_3.divide_and_conquer_subarray(test_array)
